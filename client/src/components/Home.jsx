dc2b40b0f019d71480a549b803dff1a1e8125b89
Ryan Newman <ryannewman2828@gmail.com>
2017-08-26 15:21:53 -0400

    Images working

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import Image from '../images/address.png'
 
 import '../css/home.css';
+//import '../images/address.png'
 
 class Home extends Component {
+
+   // function dirButton(props) {
+        
+   // }
 
   render() {
     return (
     	
-		    <div className="home"> Home </div>
-      
+		    <div className="home">
+                <img src={Image} />
+            </div>
     );
   }
 }
