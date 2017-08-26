7656d1536d9bcc84e148fbe4018aa7ef96744bfc
Ryan Newman <ryannewman2828@gmail.com>
2017-08-26 16:53:46 -0400

    working hello world api

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -3,5 +3,6 @@ var User = mongoose.model('User');
 
 module.exports.hello = function (req, res) {
     console.log('hello world');
-    res.status(200);
+    res.status(200).json('{ test: worked }');
+    return
 };
