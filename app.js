21d0a36f0ef44b90d5d139ce2a3d5ed5e159204b
Ryan Newman <ryannewman2828@gmail.com>
2017-08-26 13:30:57 -0400

    Database connected

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./server/routes/index');
+var users = require('./server/routes/users');
+
+require('./server/model/db');
 
 var app = express();
 
---
+++ server/model/db.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var dbURI;
+
+dbURI = 'mongodb://username:password@ds015335.mlab.com:15335/userbase';
+
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
+
