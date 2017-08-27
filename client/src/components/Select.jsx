b469d569e50847a9a8c8f09eb6f3c4db929f38ca
Ryan Newman <ryannewman2828@gmail.com>
2017-08-27 04:06:09 -0400

    MAPP!!!!!!!!!!!!!!!

--- client/src/components/Select.jsx
+++ client/src/components/Select.jsx
@@ -1,11 +1,40 @@
 import React, { Component, Text } from 'react';
-import GoogleMapReact from "google-map-react";
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import { Switch, Route, Link } from 'react-router-dom';
 
 import '../css/select.css';
 
 
 class Select extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+        }
+        // binding this to event-handler functions
+        this.onMarkerClick = this.onMarkerClick.bind(this);
+        this.onMapClicked = this.onMapClicked.bind(this);
+    }
+
+    onMarkerClick(props, marker, e) {
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+    }
+
+    onMapClicked(props) {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+        }
+    }
 
     render() {
         return (
@@ -17,7 +46,7 @@ class Select extends Component {
                             placeholder="Search..."
                         />
                         <select>
-                            <option value="Cuisine">Volvo</option>
+                            <option value="Cuisine">Cuisine</option>
                             <option value="volvo">Volvo</option>
                             <option value="saab">Saab</option>
                             <option value="mercedes">Mercedes</option>
@@ -33,11 +62,25 @@ class Select extends Component {
                     </h1>
                 </div>
                 <div>
+                    <Map google={this.props.google}
+                         onClick={this.onMapClicked}>
+                        <Marker onClick={this.onMarkerClick}
+                                name={'Current location'} />
 
+                        <InfoWindow
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}>
+                            <div>
+                                <h1>{this.state.selectedPlace.name}</h1>
+                            </div>
+                        </InfoWindow>
+                    </Map>
                 </div>
             </div>
         );
     }
 }
 
-export default Select;
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyDxL4NAv1bO7f9ofa_nl_zt2sEzG67I5Pk')
+})(Select)
